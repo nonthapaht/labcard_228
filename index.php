525915125d01262c79d8d9e474d88e02565bc782
nonthapaht <73693563+nonthapaht@users.noreply.github.com>
2020-11-06 17:28:31 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Lab</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <style>
+            body{
+                background-color: #38383ada;
+            }
+
+            h5{
+                color: black;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="card-deck">
+                <div class="card">
+                  <img src="img/veiw.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">BLUE MOON</h5>
+                    <p class="card-text">ฺพระจันทร์ที่ว่าไกลจะไปถึง ระยะทางยังสั้นกว่าระยะทางไปสู่ใจของเธอเลย เศร้านะครับ T^T</p>
+                  </div>
+                  <div class="card-footer">
+                    <img style="border-radius: 50%;" src="img/img_avatar.png" alt="Avatar" height=25>
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="red" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                      </svg>
+                  </div>
+                </div>
+                <div class="card">
+                  <img src="img/veiw1.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">SUN SET</h5>
+                    <p class="card-text">ผมหลงใหลท้องฟ้ายามตะวันตกดิน เพราะมันให้ความรู้่สึกอบอุ่น และ สวนยงามไม่เหมือนกันเลยในแต่ละวัน</p>
+                  </div>
+                  <div class="card-footer">
+                    <img style="border-radius: 50%;" src="img/img_avatar.png" alt="Avatar" height=25>
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="red" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                      </svg>
+                  </div>
+                </div>
+                <div class="card">
+                  <img src="img/veiw2.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">MIDNIGHT</h5>
+                    <p class="card-text">ค่่ำคืนที่มืดมิด และ เงียบเหงา การมีเธอที่เหมือนเป็นความสบายใจ ทำให้คืนที่มืดมิดของผมไม่เดียวดายอิกต่อไป</p>
+                  </div>
+                  <div class="card-footer">
+                    <img style="border-radius: 50%;" src="img/img_avatar.png" alt="Avatar" height=25>
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="red" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                      </svg>
+                  </div>
+                </div>
+              </div>
+          </div>
+    </body>
+</html>
